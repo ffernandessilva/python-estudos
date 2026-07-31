a24d2348271fa95767fa941860ecf7b10a2c8889
Felipe Fernandes <ffs.fernandes@outlook.com>
2026-07-31 15:54:15 -0300

    Aula021 - Interpolação de Strings

---
+++ Aula021 - Interpolação de Strings/main.py
@@ -0,0 +1,14 @@
+"""
+Interpolação básica de string
+s - string
+d e i - int
+f - float
+x e x - Hexadecimal (ABCDEF0123456789)
+
+"""
+
+nome = 'Felipe'
+preco = 1000.95897643
+variavel = '%s, o preço é R$%.2f' %(nome, preco)
+print(variavel)
+print('O hexadecimal de %d é %x' %(15, 15))
