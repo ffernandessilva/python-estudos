1ff112d179bea10e2ac284316ff4bbc033b0d9a0
Felipe Fernandes <ffs.fernandes@outlook.com>
2026-07-30 21:48:50 -0300

    Aula017 - Operador Lógico I

--- Aula017 - Operador Lógico I/main.py
+++ Aula017 - Operador Lógico I/main.py
@@ -9,10 +9,14 @@
 # usado para representar um não valor
 
 entrada = input('[E]ntrar [S]air: ')
-senha_digitada = input('Digite a senha: ')
 senha_permitida = '123456'
 
-if entrada == 'E' and senha_digitada == senha_permitida:
-    print('Entrar')    
+if entrada == 'E':
+    senha_digitada = input('Digite a senha: ')
+
+    if senha_digitada == senha_permitida:
+        print('Entrar')
+    else:
+        print('Senha incorreta')
 else:
     print('Sair')
