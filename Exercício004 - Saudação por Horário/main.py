0dfee2b74914f2ae33339aaf07ae98f31e2e8b83
Felipe Fernandes <ffs.fernandes@outlook.com>
2026-08-02 17:52:42 -0300

    Exercício004 - Saudação por Horário

---
+++ Exercício004 - Saudação por Horário/main.py
@@ -0,0 +1,20 @@
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+horario = input("Digite o horário atual: ")
+
+if horario.isdigit():
+    horario = int(horario)
+    if horario >= 18 and horario <= 23:
+        print("Boa noite!")
+    elif horario >= 12:
+        print("Boa tarde!")
+    else:
+        print("Bom dia!")
+else:
+     print("Você não digitou um número inteiro ou um número")
+
+            
