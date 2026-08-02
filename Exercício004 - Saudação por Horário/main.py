1a1c14828e5c94be8fdfe20ed90c2cff5ab9727d
Felipe Fernandes <ffs.fernandes@outlook.com>
2026-08-02 18:13:59 -0300

    Exercício004 - Saudação por Horário

--- Exercício004 - Saudação por Horário/main.py
+++ Exercício004 - Saudação por Horário/main.py
@@ -8,9 +8,11 @@ horario = input("Digite o horário atual: ")
 
 if horario.isdigit():
     horario = int(horario)
-    if horario >= 18 and horario <= 23:
+    if horario < 0 or horario > 23:
+        print("Horário inválido.")
+    elif horario >= 18 and horario <= 23:
         print("Boa noite!")
-    elif horario >= 12:
+    elif horario >= 12  and horario <= 17:
         print("Boa tarde!")
     else:
         print("Bom dia!")
